f442bbc612a1783831e753c803989c6750017d71
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-02-17 04:33:00 -0700

    Added usernames and user support

--- chat/chat.js
+++ chat/chat.js
@@ -4,9 +4,38 @@ UCID: 10159608
 Tutorial Section: 04
 */
 
+const EVENTS = {
+    CONNECT: 'connect',
+    DISCONNECT: 'disconnect',
+    USER: 'user',
+    ALL_USERS: 'all_users',
+    MESSAGE: 'message',
+    ALL_MESSAGES: 'all_messages',
+};
+
 let socket = io();
 let messagesContainer = $('#messages')[0];
-const userName = Cookies.get('user');
+let usersContainer = $('#users');
+let userName = Cookies.get('user');
+
+let sendUsername = () => {
+    socket.emit('user', {user: userName});
+}
+socket.on(EVENTS.CONNECT, sendUsername);
+socket.on(EVENTS.DISCONNECT, sendUsername);
+
+socket.on(EVENTS.USER, data => {
+    userName = data.user;
+    $("#name").text(userName);
+    Cookies.set('user', userName);
+});
+
+socket.on(EVENTS.ALL_USERS, data => {
+    usersContainer.empty();
+    for(let user of data.users) {
+        usersContainer.append(`<div class="user">${user}</div>`);
+    }
+});
 
 /* Scroll to bottom of container from https://stackoverflow.com/questions/3562202/setting-a-scrollbar-position */
 let scrollToBottom = () => {
@@ -16,6 +45,3 @@ let scrollToBottom = () => {
 }
 
 // scrollToBottom();
-
-$("#name").text(userName);
-// $("#name").text(document.cookie)
--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+/*
+Name: Inderpreet Dhillon
+UCID: 10159608
+Tutorial Section: 04
+*/
+
+const EVENTS = {
+    CONNECT: 'connection',
+    DISCONNECT: 'disconnect',
+    USER: 'user',
+    ALL_USERS: 'all_users',
+    MESSAGE: 'message',
+    ALL_MESSAGES: 'all_messages',
+};
+
 let express = require('express');
 let path = require('path');
 let bodyParser = require('body-parser');
 let cookieParser = require('cookie-parser');
+let fs = require('fs');
 
 let app = express();
 let server = require('http').Server(app);
@@ -15,24 +31,49 @@ app.use(cookieParser());
 let users = [];
 let messages = [];
 
+let possibleUsernames = [];
+fs.readFile('names.txt', (err, data) => {
+    if (err) {
+        console.error(`Error reading names: ${err}`);
+        possibleUsernames = ['User'];
+    } else {
+        possibleUsernames = data.toString().split('\n');
+    }
+});
+
 let getRandomUsername = () => {
-    return 'TestName';
+    let num = Math.floor(Math.random() * 9000) + 1000;
+    let idx = Math.floor(Math.random() * possibleUsernames.length);
+    let name = `${possibleUsernames[idx]}#${num}`;
+    if (users.indexOf(name) !== -1) {
+        return getRandomUsername();
+    }
+    return name;
 }
 
 app.get('/', (req, res) => {
-    console.log(req.cookies);
-    if (!req.cookies['user']) {
-        res.cookie('user', getRandomUsername());
-    }
     res.sendFile(path.join(__dirname + '/chat/chat.html'));
-
 });
 
 server.listen(80, () => {
     console.log('Chat listening on port 80');
 });
 
-io.on('connection', socket => {
-    console.log(`Connected to ${socket.id}`);
-    // socket.emit('retrieve', {messages: 'Server Data'});
+io.on(EVENTS.CONNECT, socket => {
+    console.log('>> Socket connected');
+    socket.on(EVENTS.USER, data => {
+        if (!data.user) {
+            data.user = getRandomUsername();
+        }
+        if (users.indexOf(data.user) == -1) {
+            users.push(data.user);
+        }
+        socket.emit(EVENTS.USER, data);
+        io.emit(EVENTS.ALL_USERS, {users: users.sort()});
+    });
+    socket.on(EVENTS.DISCONNECT, () => {
+        console.log('<< Socket disconnected');
+        users = [];
+        io.emit(EVENTS.DISCONNECT);
+    });
 });
