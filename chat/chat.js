84b006f6d95bb7ac134e618036b6a4caa96b1ebb
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-02-17 05:47:44 -0700

    Added disconnect handling, nick names

--- chat/chat.js
+++ chat/chat.js
@@ -11,12 +11,21 @@ const EVENTS = {
     ALL_USERS: 'all_users',
     MESSAGE: 'message',
     ALL_MESSAGES: 'all_messages',
+    NICK_NAME: 'nick',
 };
 
 let socket = io();
-let messagesContainer = $('#messages')[0];
+let messagesContainer = $('#messages');
 let usersContainer = $('#users');
 let userName = Cookies.get('user');
+let inputArea = $('#input')
+let errorArea = $('#error');
+let nickcolor = 'blue';
+
+let showError = errorMessage => {
+    errorArea.show();
+    errorArea.text(`Error: ${errorMessage}`);
+}
 
 let sendUsername = () => {
     socket.emit('user', {user: userName});
@@ -24,11 +33,20 @@ let sendUsername = () => {
 socket.on(EVENTS.CONNECT, sendUsername);
 socket.on(EVENTS.DISCONNECT, sendUsername);
 
-socket.on(EVENTS.USER, data => {
+let setName = data => {
     userName = data.user;
     $("#name").text(userName);
     Cookies.set('user', userName);
-});
+}
+socket.on(EVENTS.USER, setName);
+socket.on(EVENTS.NICK_NAME, data => {
+    if (data.result) {
+        setName(data);
+        inputArea.val('');
+    } else {
+        showError('Nickname already in use')
+    }
+})
 
 socket.on(EVENTS.ALL_USERS, data => {
     usersContainer.empty();
@@ -37,11 +55,39 @@ socket.on(EVENTS.ALL_USERS, data => {
     }
 });
 
+inputArea.keypress(event => {
+    if (event.charCode === 13) {
+        errorArea.hide();
+        let message = inputArea.val().toString();
+        if (message.startsWith('/nick ')) {
+            let splitted = message.split(' ');
+            if (splitted.length !== 2) {
+                showError('Incorrect number of arguments, expected 1');
+            } else {
+                let data = {
+                    current_name: userName,
+                    new_name: splitted[1],
+                }
+                socket.emit(EVENTS.NICK_NAME, data);
+            }
+        } else if (message.startsWith('/nickcolor ')) {
+            let splitted = message.split(' ');
+            if (splitted.length !== 2) {
+                showError('Incorrect number of arguments, expected 1');
+            } else {
+                nickcolor = splitted[1];
+            }
+        } else if (message.startsWith('/')) {
+            showError('Invalid command');
+        }
+    }
+});
+
 /* Scroll to bottom of container from https://stackoverflow.com/questions/3562202/setting-a-scrollbar-position */
 let scrollToBottom = () => {
     let messages = $('.message');
     let bottomRow = messages[messages.length - 1];
-    messagesContainer.scrollTop = bottomRow.offsetTop;
+    messagesContainer[0].scrollTop = bottomRow.offsetTop;
 }
 
-// scrollToBottom();
+scrollToBottom();
--- index.js
+++ index.js
@@ -5,12 +5,13 @@ Tutorial Section: 04
 */
 
 const EVENTS = {
-    CONNECT: 'connection',
+    CONNECT: 'connect',
     DISCONNECT: 'disconnect',
     USER: 'user',
     ALL_USERS: 'all_users',
     MESSAGE: 'message',
     ALL_MESSAGES: 'all_messages',
+    NICK_NAME: 'nick',
 };
 
 let express = require('express');
@@ -62,18 +63,30 @@ server.listen(80, () => {
 io.on(EVENTS.CONNECT, socket => {
     console.log('>> Socket connected');
     socket.on(EVENTS.USER, data => {
-        if (!data.user) {
+        if (!data.user || users.indexOf(data.user) >= 0) {
             data.user = getRandomUsername();
         }
-        if (users.indexOf(data.user) == -1) {
-            users.push(data.user);
-        }
+        users.push(data.user);
         socket.emit(EVENTS.USER, data);
         io.emit(EVENTS.ALL_USERS, {users: users.sort()});
+        // io.emit(EVENTS.ALL_MESSAGES, {messages: messages});
     });
     socket.on(EVENTS.DISCONNECT, () => {
         console.log('<< Socket disconnected');
         users = [];
         io.emit(EVENTS.DISCONNECT);
     });
+    socket.on(EVENTS.NICK_NAME, data => {
+        if (users.indexOf(data.new_name) >= 0) {
+          socket.emit(EVENTS.NICK_NAME, {result: false});
+        } else {
+            /* Remove element from https://stackoverflow.com/questions/5767325/how-do-i-remove-a-particular-element-from-an-array-in-javascript */
+            let oldIdx = users.indexOf(data.current_name);
+            users.splice(oldIdx, 1);
+            users.push(data.new_name);
+
+            socket.emit(EVENTS.NICK_NAME, {result: true, user: data.new_name});
+            io.emit(EVENTS.ALL_USERS, {users: users.sort()});
+        }
+    })
 });
